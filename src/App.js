6b77bc1a38bca9138d2810a0b9fc28accaf59a80
Himmelzkind <73137185+Himmelzkind@users.noreply.github.com>
2021-01-26 19:08:14 +0100

    Added the Git Hub url as footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <div className="App">
       weather app
+      <footer> <a href="https://github.com/Himmelzkind/final-project-react.git" target="_blank">Open source code</a></footer>
+     
     </div>
   );
 }
