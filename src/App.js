46158ca82026e512eaac8b0a0b73d935aed00bf0
Himmelzkind <73137185+Himmelzkind@users.noreply.github.com>
2021-01-27 17:22:52 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      weather app
-      <footer> <a href="https://github.com/Himmelzkind/final-project-react.git" target="_blank">Open source code</a></footer>
-     
+      <div className="container">
+        <h1>weather app app</h1>
+        <footer> <a href="https://github.com/Himmelzkind/final-project-react.git" target="_blank">Open source code</a></footer>
+      </div>
+      
     </div>
   );
 }
