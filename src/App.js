13f240515a9c48b4ee7a7b4a1a9bf8e8b945cc3c
Himmelzkind <73137185+Himmelzkind@users.noreply.github.com>
2021-02-02 21:39:27 +0100

    Add weather API
    iAdded openweather API and useState

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>weather app app</h1>
-        <Weather />
+        <Weather defaultCity="Tokyo"/>
         <footer> <a href="https://github.com/Himmelzkind/final-project-react.git" target="_blank" rel="noreferrer">Open source code</a> by Laura Himmelreich</footer>
       </div>
       
--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import "./weather.css";
 import Search from "./Search.js";
+import axios from "axios";
 
-export default function Weather(){
-    return(
-    <div className="Weather">
-        <Search />
-        <h1>Lisbon</h1>
-        <ul>
-            <li>Wednsday 7:00</li>
-            <li>mostly cloudy</li>
-        </ul>
-        <div className="weatherToday">
-            <img id="icon-main" alt="weathericon" src=""/>
-            <strong id="currentTemp">20</strong>
-            <span className="units">
-                <button className="celsius-link" href="/">°C</button>
-                <button className="fahrenheit-link" href="#">°F</button>
-            </span>
-            <p id="humidity_wind">humidity | windspeed</p>
-        </div>
-        <div class="row forecast_bottom">
-         
-        </div>
-    </div>);
+export default function Weather(props){
+
+    const [weatherData, setWeatherData] = useState({ready:false});
+    function handleSubmit(response){
+        console.log(response.data.name);
+        setWeatherData({
+            ready: true,
+            city: response.data.name,
+            date: "Wednesday 7:00",
+            temperature: Math.round(response.data.main.temp),
+            humidity: Math.round(response.data.main.humidity),
+            description: response.data.weather[0].description,
+            windSpeed: Math.round(response.data.wind.speed),
+            iconUrl: "https://icons-for-free.com/iconfiles/png/512/cloud+day+forecast+lightning+shine+storm+sun+weather+icon-1320183295537909806.png"
+        });
+    }
+
+    if (weatherData.ready){
+        return (
+            <div className="Weather">
+                <Search />
+                <h1>{weatherData.city}</h1>
+                <h2>{weatherData.date}</h2>
+
+    
+                <div className="weatherToday">
+                    <img id="icon-main" src={weatherData.iconUrl} alt={weatherData.description} />
+                    <strong id="currentTemp">{weatherData.temperature}</strong>
+                    <span className="units">{weatherData.temperature}
+                        <button className="celsius-link">°C</button>
+                        <button className="fahrenheit-link">°F</button>
+                    </span>
+                    <p className="humidityWind">humidity: {weatherData.humidity} % | windspeed: {weatherData.windSpeed} km/h</p>
+                </div>
+
+                <div className="row forecast_bottom"></div>
+                
+            </div>)
+    } else {
+        const apiKey = "361119f7d767ce895ccf917d2e91cc83";
+        let apiUrl =`http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleSubmit);
+        return (
+            "loading..."
+        )
+    }
+    
+    
 }
